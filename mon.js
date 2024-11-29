82525e6766910f7130a2067f1d5bc16ae2f2ddf0
Evrad123 <ngoumouevrad33@gmail.com>
2024-11-29 13:14:44 +0100

    Add files via upload

---
+++ mon.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const ctaBtn = document.querySelector('.cta-btn');
+    
+    // Ajouter un effet au bouton CTA
+    ctaBtn.addEventListener('click', function() {
+        aler
